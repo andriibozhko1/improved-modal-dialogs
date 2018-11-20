f9111191db9a67f130839b3173adcfd6b293de44
Andrii <andriibozhko1@gmail.com>
2018-11-20 12:47:18 +0200

    remove errors

--- src/js/script.js
+++ src/js/script.js
@@ -1,51 +1,37 @@
 (function() {
   const modal = document.querySelectorAll(".modal");
-  const modalContent = document.querySelectorAll('.modal__content')
-  const openModalBtn = document.querySelectorAll(".open-modal-dialog");
+  const modalContent = document.querySelectorAll(".modal__content");
+  const openModalBtns = document.querySelectorAll(".open-modal-dialog");
   const primary = document.querySelectorAll("[data-type=btn-primary]");
   const closeBtns = document.querySelectorAll("[data-type=btn-close]");
-  const createBackground = document.createElement("div");
-  const closeBtn = document.createElement("a");
 
-  for (let j = 0; j < openModalBtn.length; j++) {
-    openModalBtn[j].addEventListener("click", function(event) {
-        const openModal = function() {
-          modal[j].classList.remove("hide");
-          document.body.insertBefore(createBackground, modal[j]);
-          createBackground.classList.add("overlay");
-        };
-        const closeDialog = function() {
-          modal[j].classList.add("hide");
-          createBackground.remove();
-          closeBtn.remove();
-        };
-        const addCloseBtn = function() {
-          closeBtn.classList.add("btn-closeBtn");
-          closeBtn.setAttribute("data-type", "btn-close");
-          closeBtn.innerHTML =
-            '<img class="modal__icon--closeBtn" src="img/icon-popup-close.svg" alt="close" />';
-          modal[j].insertBefore(closeBtn,modalContent[j]);
-        };
-        openModal();
-        addCloseBtn();
-        closeBtn.addEventListener("click", function() {
-          closeDialog();
-        });
-        for (let i = 0; i < closeBtns.length; i++) {
-          closeBtns[i].addEventListener("click", function() {
-            closeDialog();
-          });
-          createBackground.addEventListener("click", function(event) {
-              closeDialog();
-          });
-        }
-        primary[j].addEventListener("click", function() {
-          closeDialog();
-          const WindowWithMsg = function() {
-            alert("DONE :)");
-          };
-          setTimeout(WindowWithMsg, 500);
-        });
+  for (let j = 0; j < openModalBtns.length; j++) {
+    openModalBtns[j].addEventListener("click", function() {
+      const createBackground = document.createElement("div");
+      const crossBtn = document.createElement("a");
+      const openModal = function() {
+        modal[j].classList.remove("hide");
+        document.body.insertBefore(createBackground, modal[j]);
+        createBackground.classList.add("overlay");
+      };
+      const closeDialog = function() {
+        modal[j].classList.add("hide");
+        createBackground.remove();
+        crossBtn.remove();
+      };
+      const addCrossBtn = function() {
+        crossBtn.classList.add("btn-closeBtn");
+        crossBtn.setAttribute("data-type", "btn-close");
+        crossBtn.innerHTML =
+          '<img class="modal__icon--closeBtn" src="img/icon-popup-close.svg" alt="close" />';
+        modal[j].insertBefore(crossBtn, modalContent[j]);
+      };
+
+      openModal();
+      addCrossBtn();
+      closeBtns[j].addEventListener("click", closeDialog);
+      crossBtn.addEventListener("click", closeDialog);
+      createBackground.addEventListener("click", closeDialog);
     });
   }
 })();
